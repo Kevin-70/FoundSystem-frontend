16f1ecd7ad2698ea7d146bbe5c1de74241765583
hermiton <12010602@mail.sustech.edu.cn>
2023-05-18 21:27:26 +0800

    add BarGraph

---
+++ src/utils/graphHelper.js
@@ -0,0 +1,11 @@
+export default {
+    applications2Line(applications) {
+        let x_data = [];
+        let y_data = [];
+        for (let i = 0; i < applications.length; i++) {
+            x_data.push(applications[i].createdDate);
+            y_data.push(applications[i].appAmount);
+        }
+        return x_data, y_data;
+    }
+}
